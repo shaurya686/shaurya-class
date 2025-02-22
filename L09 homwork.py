7ee1d9c4cd4026fc04ea9a32f2bd0610c7b55d43
shaurya686 <shauryakapadia08@gmail.com>
2025-02-22 12:26:49 -0600

    L09 homework

---
+++ L09 homwork.py
@@ -0,0 +1,10 @@
+age = int(input("enter your age = "))
+
+if age > 10:
+    print("class are for age 10 to 20")
+    if age >= 10:
+        print(" your are allowed to take class")
+    else:
+        print("you are no allowed to take class")
+else:
+    print("you are too young")
