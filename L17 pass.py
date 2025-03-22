7edc59cc8782ea04547a609975ead557daf5bda4
shaurya686 <shauryakapadia08@gmail.com>
2025-03-22 12:30:02 -0500

    L17 pass

--- L17 pass.py
+++ L17 pass.py
@@ -3,4 +3,13 @@ for x in range(10):
         print("twist")
 
     elif x % 15 == 0:
-        pass # ingore or si
+        pass # ingore or skip this code
+
+    elif x % 5 == 0:
+        print("fizz")
+
+    elif x % 3 == 0:
+        print("buzz")
+
+    else:
+        print(x)
