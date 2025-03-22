fb85e64929b6a4ebb743d01a6cc7242ffa722abb
shaurya686 <shauryakapadia08@gmail.com>
2025-03-22 12:17:25 -0500

    L17 break

---
+++ L17 break.py
@@ -0,0 +1,8 @@
+word = input("enter a word = ")
+
+for i in word:
+    if (i == 'A'):
+        print("A is found")
+        break #stop the repetition process and get out of loop
+    else:
+        print("A is not found") 
---
+++ L17 pass.py
@@ -0,0 +1,6 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("twist")
+
+    elif x % 15 == 0:
+        pass # ingore or si
