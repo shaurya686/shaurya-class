12f793c693c8c94055349ce485adc31310946942
shaurya686 <shauryakapadia08@gmail.com>
2025-03-30 10:43:19 -0500

    L19 homework

---
+++ L19 homework.py
@@ -0,0 +1,11 @@
+import math
+
+x = int(input("enter a trignometric value: "))
+
+a = math.sin(x)
+b = math.cos(x)
+c = math.tan(x)
+
+print("value of sin: ",a)
+print("value of cos: ",b)
+print("value of tan: ",c)
