e79ada6079ee92aed1db4585813ad948395cd0b1
shaurya686 <shauryakapadia08@gmail.com>
2025-04-12 12:58:58 -0500

    L23hw

---
+++ L23hw.py
@@ -0,0 +1,20 @@
+test_dict = {'codingal' : 2,
+             'is' : 2,
+             'best' : 2,
+             'for' : 3,
+             'coding' : 1}
+
+#printing original dictionary
+print("The original dictionary : " + str(test_dict))
+
+#Initialize value 
+K = int(input("enter K value : "))
+
+#using loop
+#selective key value in dictionary 
+res = 0 
+for key in test_dict: 
+    if test_dict[key] == K:
+        res = res + 1
+#printing result 
+print("Frequecny of K is :" + str(res))
